ee5f3a9a25f8f987a4abe6cb9da5cbe9437917b3
UnknownAnt <1246768688@qq.com>
2025-03-16 18:48:35 +0800

    Site updated: 2025-03-16 18:48:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/13/LearningTips/","2025/03/08/hello-world/"];function toRandomPost(){
+var posts=["2025/03/08/hello-world/","2025/03/13/LearningTips/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
