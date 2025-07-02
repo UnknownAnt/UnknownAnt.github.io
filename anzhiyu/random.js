176875b4b5580a5e2aa9bafb1ab9beaa4f60506c
UnknownAnt <1246768688@qq.com>
2025-07-02 12:11:55 +0800

    Site updated: 2025-07-02 12:11:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/08/hello-world/","2025/03/13/LearningTips/","2025/03/18/数据库系统概论（第六版）/","2025/04/26/追风的石头/","2025/03/17/timeLine/"];function toRandomPost(){
+var posts=["2025/03/13/LearningTips/","2025/03/08/hello-world/","2025/03/17/timeLine/","2025/07/02/考前突击笔记/","2025/03/18/数据库系统概论（第六版）/","2025/04/26/追风的石头/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
